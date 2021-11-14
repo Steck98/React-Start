3ec84d0d032726252dada870070492f8006cf59a
Steck98 <steczkiewicz1998@Gmail.com>
2021-11-14 21:44:39 +0100

    Fix Heroku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+let port = process.env.PORT;
+if (port == null || port == '') {
+  port = 8000;
+}
 import React from 'react';
 import './styles/normalize.scss';
 import './styles/global.scss';
@@ -6,10 +10,7 @@ import App from './components/App/AppContainer';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-let port = process.env.PORT;
-if (port == null || port == '') {
-  port = 8000;
-}
+
 App.listen(port);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
